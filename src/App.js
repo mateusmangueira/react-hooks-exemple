b3dc6cbc9c3c2ea4358e8fd6cf45442ede7fc767
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-11 19:27:10 -0300

    Adicionando useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState([]);
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+        placeholder="Add a new tchnology"
+      />
+      <button type="button" onClick={handleAdd}>
+        Add new tech
+      </button>
+    </>
+  );
 }
 export default App;
